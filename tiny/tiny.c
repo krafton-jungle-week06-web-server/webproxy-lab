9048dbedba6397a8f6e29ccdb7efd626963b6711
Jo Youjin <youjinjoy@gmail.com>
2023-11-18 06:26:05 +0000

    added doit, clienterror, read_requesthdrs, parse_uri

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -41,3 +41,117 @@ int main(int argc, char **argv) {
     Close(connfd);  // line:netp:tiny:close
   }
 }
+
+void doit(int fd)
+{
+  int is_static;
+  struct stat sbuf;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t rio;
+
+  // request line과 header를 읽는다.
+  Rio_readintitb(&rio, fd);
+  Rio_readlineb(&rio, buf, MAXLINE);
+  printf("Request headers:\n");
+  printf("%s",buf);
+  sscanf(buf, "%s %s %s", method, uri, version);
+  if (strcasecmp(method, "GET")){ // method가 GET이 아닐 때
+    clienterror(fd, method, "501", "Not implemneted", "Tiny does not implement this method");
+    return;
+  }
+  read_requesthdrs(&rio);
+
+  // GET 요청으로부터 URI 분할하기
+  is_static = parse_uri(uri, filename, cgiargs);
+  if (stat(filename, &sbuf)<0){ // 파일이 없으면? 인 듯
+    clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+    return;
+  }
+
+  if (is_static){ // 정적 컨텐츠 serve하기
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)){ // 파일이 있어도 접근할 수 없다면? 인 듯
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+      return;
+    }
+    serve_staic(fd, filename, sbuf.st_size);
+  }
+  else{ // 동적 컨텐츠 serve하기
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)){ // 파일이 있어도 접근할 수 없다면? 인 듯
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
+      return;
+    }
+    serve_dynamic(fd, filename, cgiargs);
+  }
+
+}
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+{
+  char buf[MAXLINE], body[MAXBUF];
+
+  // HTTP response body 빌드하기
+  sprintf(body, "<html><title>Tiny Error</title>");
+  sprintf(body, "%s<body bgcolor=""ffffffff""\r\n", body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+
+  // HTTP response 출력하기
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+  Rio_writen(fd, body, strlen(body));
+}
+
+void read_requesthdrs(rio_t *rp) // tiny는 요청 헤더 내의 어떤 정보도 사용하지 않는다. 헤더를 읽고 무시하는 함수.
+{ // rp는 request header pointer 인듯
+  char buf[MAXLINE];
+
+  Rio_readlineb(rp, buf, MAXLINE);
+  while(strcmp(buf, "\r\n")){ // 버퍼의 끝까지 읽기만 하기
+    Rio_readlineb(rp, buf, MAXLINE);
+    printf("%s", buf);
+  }
+  return;
+}
+
+int parse_uri(char *uri, char *filename, char *cgiargs)
+{
+  char *ptr;
+
+  if (!strstr(uri, "cgi-bin")){ // 정적 컨텐츠
+    strcpy(cgiargs, "");
+    strcpy(filename, ".");
+    strcat(filename, uri);
+    if (uri[strlen(uri)-1] == '/'){
+      strcat(filename, "home.html");
+    }
+    return 1;
+  }
+  else{ // 동적 컨텐츠 
+    ptr = index(uri, '?'); // uri에서 ? 위치(인덱스) 뽑아줌
+    if (ptr){
+      strcpy(cgiargs, ptr+1);
+      *ptr='\0';
+    }
+    else{
+      strcpy(cgiargs, "");
+    }
+    strcpy(filename, ".");
+    strcat(filename, uri);
+    return 0;
+  }
+}
+
+void serve_static(int fd, char *filename, int filesize)
+{}
+
+void get_filetype(char *filename, char *filetype)
+{}
+
+void serve_dynamic(int fd, char *filename, char *cgiargs)
+{}
